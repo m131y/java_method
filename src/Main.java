1b863db3bf8f6250c236191949652dc29c0c1421
whals <whalsdud0131@gmail.cm>
2025-06-05 15:43:56 +0900

    return

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
-    public static void printStudent(String name, int age) {
-        System.out.println("이름: " + name);
-        System.out.println("나이: " + age);
-        System.out.println("------------");
+    public static int addNumber(int a, int b) {
+        int result = a + b;
+
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println("=== 학생 정보 출력 ===");
-        Main.printStudent("Alice", 20);
-        printStudent("Bob", 22);
-        printStudent("Charlie", 25);
+        int number = addNumber(10,7);
+        System.out.println(number);
     }
 }
